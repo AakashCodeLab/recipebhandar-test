f385f43fe48b225f6ada4fb6a96b6a835eb25f26
aakash1801 <singh.aakash1801@gmail.com>
2019-06-06 06:30:24 +0530

    added myrecipe

---
+++ src/app/pages/myrecipe/myrecipe.component.ts
@@ -0,0 +1,131 @@
+import {Component, AfterViewInit, OnInit} from '@angular/core';
+import {RecipedataService} from '../../services/recipedata.service';
+import {Router} from '@angular/router';
+import Recipe from '../../Recipe';
+import { NgxSpinnerService } from 'ngx-spinner';
+import {MatSnackBar} from '@angular/material';
+@Component({
+  templateUrl: './myrecipe.component.html',
+  styleUrls: ['./myrecipe.component.scss']
+})
+export class MyrecipeComponent implements  OnInit , AfterViewInit {
+  showSearchOnHeader = true;
+  viewRecipePage = false;
+  recipeData: Recipe[];
+  reacipeDetails: Recipe;
+  copyRecpeData: Recipe[];
+  userid = '';
+  userName = '';
+  instructionArray = [];
+    constructor(public router: Router, private recipeservice: RecipedataService ,
+                private spinner: NgxSpinnerService, private snackbar: MatSnackBar) {
+    }
+  ngOnInit() {
+    this.spinner.show();
+    if (localStorage.getItem('currentuser')) {
+      const userObj = JSON.parse(localStorage.getItem('currentuser'));
+      this.userid = userObj.id;
+      this.userName = userObj.userName;
+
+      // this.getData(this.userid);
+      this.getData(this.userid);
+
+    }
+
+    this.searchRecipe();
+  }
+
+  getData(userid) {
+    this.showSearchOnHeader = true;
+    this.spinner.show();
+    this.recipeservice
+      .getMyRecipe(userid)
+      .subscribe((data: Recipe[]) => {
+        this.recipeData = data;
+        this.copyRecpeData = this.recipeData;
+        this.spinner.hide();
+        if (this.router.url === '/') {
+          this.router.navigate(['/home']);
+        }
+      });
+  }
+
+  searchRecipe() {
+    this.showSearchOnHeader = true;
+    this.spinner.show();
+    this.recipeservice
+      .getRecipeOnSearch()
+      .subscribe((data: Recipe[]) => {
+        console.log(data);
+        if (data !== null && data.length > 0) {
+          this.recipeData = data;
+          this.spinner.hide();
+        }else if (data !== null && data.length === 0) {
+
+
+          setTimeout(() => {
+            this.recipeData = this.copyRecpeData;
+            const snack = this.snackbar.open('Not Found', '', {
+              duration: 1000,
+              panelClass: ['red-snackbar']
+            });
+          }, 1500);
+
+        }
+        if (this.router.url === '/') {
+          this.router.navigate(['/home']);
+        }
+      }, (err) => {
+        const snack = this.snackbar.open('Sorry ,for server error', '', {
+          duration: 500,
+          panelClass: ['red-snackbar']
+        });
+        console.log(err);
+      });
+  }
+
+
+  deleteRecipe(id) {
+    this.recipeservice.deleteRecipe(id).subscribe(res => {
+      console.log('Deleted');
+      this.getData(this.userid);
+    });
+  }
+
+  viewRecipe(recipe) {
+    this.showSearchOnHeader = false;
+      console.log(recipe);
+    this.viewRecipePage = true;
+    this.reacipeDetails = recipe;
+    this.reacipeDetails.preptime = this.convertMinsToHrsMins(this.reacipeDetails.preptime);
+    this.reacipeDetails.cooktime = this.convertMinsToHrsMins(this.reacipeDetails.cooktime);
+    this.instructionArray = this.reacipeDetails.instructions.split('.');
+    const length = this.instructionArray.length;
+    this.instructionArray = this.instructionArray.slice(0, length - 1);
+  }
+
+  GoToListRecipe() {
+    this.viewRecipePage = false;
+  }
+
+  edit( recipeId) {
+      console.log(recipeId);
+    this.router.navigate(['/edit', recipeId]);
+  }
+
+   convertMinsToHrsMins(mins) {
+    let h: any = Math.floor(mins / 60);
+    let m: any = mins % 60;
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    if (h === '00') {
+      return `${m}min`;
+    }else if (m === '00') {
+      return `${h}hrs`;
+    } else {
+      return `${h}hrs:${m}min`;
+
+    }
+  }
+  ngAfterViewInit() {}
+}
---
+++ src/app/pages/myrecipe/myrecipe.module.ts
@@ -0,0 +1,90 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { MyrecipeComponent } from './myrecipe.component';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
+
+const routes: Routes = [{
+	path: '',
+	data: {
+        title: 'Starter Page',
+        urls: [{title: 'Dashboard', url: '/'}, {title: 'Starter Page'}]
+    },
+	component: MyrecipeComponent
+}];
+
+@NgModule({
+	imports: [
+	  FormsModule,
+    CommonModule,
+    NgxSpinnerModule,
+    RouterModule.forChild(routes),
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatStepperModule,
+    ],
+	declarations: [MyrecipeComponent]
+})
+export class MyrecipeModule { }
